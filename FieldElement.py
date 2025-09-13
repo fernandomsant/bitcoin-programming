9fb8e3e96c4cefe6818563f67168102275bc2600
Fernando <fernando.morais0703@gmail.com>
2025-09-13 10:29:34 -0300

    Implementing Field operations on FieldElements and group operations on Points

--- FieldElement.py
+++ FieldElement.py
@@ -10,6 +10,8 @@ class FieldElement:
         return f'FieldElement_{self.prime}({self.num})'
     
     def __eq__(self, other):
+        if not isinstance(other, FieldElement):
+            return False
         self.check_fields_order(other)
         return self.num == other.num
 
@@ -20,6 +22,10 @@ class FieldElement:
             return
         else:
             raise ValueError("Different field order elements")
+
+    def __neg__(self):
+        inverse = -1 * self
+        return inverse
 
     def __add__(self, other):
         self.check_fields_order(other)
@@ -32,12 +38,33 @@ class FieldElement:
         return FieldElement(num, self.prime)
     
     def __mul__(self, other):
-        self.check_fields_order(other)
-        num = (self.num * other.num) % self.prime
-        return FieldElement(num, self.prime)
+        if isinstance(other, FieldElement):
+            self.check_fields_order(other)
+            num = (self.num * other.num) % self.prime
+            return FieldElement(num, self.prime)
+        if isinstance(other, int):
+            num = (self.num * other) % self.prime
+            return FieldElement(num, self.prime)
+        raise ValueError('Multiply only by the field elements itself or integers')
+    
+    def __rmul__(self, other):
+        return self.__mul__(FieldElement(other % self.prime, self.prime))
     
     def __truediv__(self, other):
-        self.check_fields_order(other)
-        other_num_inverse = other.num ** (self.prime - 2)
-        num = (self.num * other_num_inverse) % self.prime
+        if isinstance(other, FieldElement):
+            self.check_fields_order(other)
+            other_inverse = pow(other, (self.prime - 2))
+        elif isinstance(other, int):
+            other_inverse = pow(other, (self.prime - 2), self.prime)    
+        else:
+            raise ValueError('Divide only by the field elements itself or integers')
+        return (self * other_inverse)
+    
+    def __rtruediv__(self, other):
+        return self.__truediv__(FieldElement(other % self.prime, self.prime))
+    
+    def __pow__(self, other):
+        if not isinstance(other, int):
+            raise ValueError('Potentiation only defined on integers set')
+        num = pow(self.num, other, self.prime)
         return FieldElement(num, self.prime)
--- Point.py
+++ Point.py
@@ -5,9 +5,63 @@ class Point:
         self.y = y
         self.a = a
         self.b = b
+
+        if x != None and y != None and (y**2 != x**3 + a*x + b):
+            raise ValueError('The point does not correspond the equation')
+        return
     
     def __repr__(self):
         return f'Point({self.x}, {self.y})'
     
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+    
+    def check_equation(self, other):
+        if not isinstance(other, Point):
+            raise ValueError('The operand is not a Point instance')
+        if (self.a == other.a and self.b == other.b):
+            return
+        else:
+            raise ValueError('The points does not correspond the equations')
+
+    def __neg__(self):
+        inverse = Point(self.x, -self.y, self.a, self.b)
+        return inverse
+
+    def __add__(self, other):
+        self.check_equation(other)
+        x1, x2, y1, y2 = self.x, other.x, self.y, other.y
+        if x1 == None:
+            return other
+        if x2 == None:
+            return self
+        if x1 == x2 and y1 != y2:
+            return Point(None, None, self.a, self.b)
+        if self == other:
+            s = (3 * (x1**2) + self.a) / (2 * y1)
+            x3 = s**2 - 2 * x1
+            y3 = -(s * (x3 - x1) + y1)
+            return Point(x3, y3, self.a, self.b)
+        s = (y2 - y1) / (x2 - x1)
+        x3 = s**2 - x1 - x2
+        y3 = -(s * (x3 - x1) + y1)
+        return Point(x3, y3, self.a, self.b)
+    
+    def __sub__(self, other):
+        self.check_equation(other)
+        return self + (-other)
+    
+    def __mul__(self, other):
+        if not isinstance(other, int):
+            raise ValueError('The factor must be an integer')
+        result = Point(None, None, self.a, self.b)
+        if other > 0:
+            for _ in range(0, other):
+                result = result + self
+        if other < 0:
+            for _ in range(0, other):
+                result = result - self
+        return result
+    
+    def __rmul__(self, other):
+        return self * other
