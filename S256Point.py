4c96b5bdbe3e42a3ef23af48fc15be495d46d982
Fernando <fernando.morais0703@gmail.com>
2025-09-14 18:43:54 -0300

    Add field and point classes for bitcoin parameters

---
+++ S256Point.py
@@ -0,0 +1,29 @@
+A = 0
+B = 7
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+from Point import Point
+from S256Field import S256Field
+
+class S256Point(Point):
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+    
+    def __rmul__(self, other):
+        other_mod = other % N
+        return super().__rmul__(other_mod)
+    
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N-2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+    
+G = S256Point(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+              0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
